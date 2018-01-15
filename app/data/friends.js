921d9ef34eb754a5a099d83d49e9354377eed03b
Mike Troutman <mktrout09@gmail.com>
2018-01-15 11:03:02 -0500

    added survey questions and friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,9 +2,89 @@ var friends = [
   {
     "name": "Ahmed",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": []
+    "scores": [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+
+    {
+    "name": "John Armitage",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+      "4",
+      "2",
+      "3",
+      "5",
+      "5",
+      "2",
+      "1",
+      "5",
+      "3",
+      "3"
+    ]
+  },
+
+    {
+    "name": "William Dafoe",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+      "1",
+      "1",
+      "3",
+      "5",
+      "5",
+      "2",
+      "4",
+      "1",
+      "2",
+      "5"
+    ]
+  },
+
+    {
+    "name": "Mike Savage",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+      "5",
+      "5",
+      "1",
+      "1",
+      "3",
+      "4",
+      "1",
+      "5",
+      "3",
+      "2"
+    ]
+  },
+
+    {
+    "name": "Tristan Jones",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+      "1",
+      "4",
+      "5",
+      "2",
+      "3",
+      "5",
+      "4",
+      "2",
+      "5",
+      "3"
+    ]
+  },
     
-  }
+ 
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
